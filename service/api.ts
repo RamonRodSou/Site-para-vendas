29cc2b67239a38a5f3154e6fc0a9e776c0e72ca0
Ramon Rodrigues <ramonrodsouza@gmail.com>
2026-01-25 13:53:48 -0300

    feature-003 - versao 1

--- service/api.ts
+++ service/api.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-const supabaseUrl = "https://loerotzzthiraxkqadnf.supabase.co"
-const supabaseKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxvZXJvdHp6dGhpcmF4a3FhZG5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2MjY4NTMsImV4cCI6MjA3NjIwMjg1M30.jsjBzHN6HAofKy-7AJdUWgVuL_WhWpBddD4w2uU-DIQ"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
     throw new Error("Supabase URL e Key são obrigatórios no .env");
